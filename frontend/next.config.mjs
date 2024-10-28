c4fb23adbe948531e9e12f68ef39b049725240b6
anrysys <anrysys@gmail.com>
2024-10-28 13:02:50 +0200

    Add babel-loader

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.module.rules.push({
+          test: /react-toastify\.esm\.mjs$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                sourceMaps: false,
+              },
+            },
+          ],
+        });
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
