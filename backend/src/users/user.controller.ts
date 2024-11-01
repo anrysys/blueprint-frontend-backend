d3c07a968fc820bac7fb9b5fadff075f1f2dcf6e
anrysys <anrysys@gmail.com>
2024-11-01 15:33:03 +0200

    Убрать класс UserResponse и использовать деструктуризацию для исключения пароля из ответа пользователя

--- backend/src/users/user.controller.ts
+++ backend/src/users/user.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
-import { Exclude, instanceToPlain } from 'class-transformer'; // Импортируем class-transformer
 import { Request, Response } from 'express';
 import { AuthGuard } from '../auth/auth.guard';
 import { User } from './user.entity'; // Импортируем сущность User
@@ -7,19 +6,6 @@ import { UsersService } from './users.service';
 
 interface AuthenticatedRequest extends Request {
   user: User; // Добавляем типизацию для req.user
-}
-
-class UserResponse {
-  id: number;
-  username: string;
-  email: string;
-
-  @Exclude()
-  password: string;
-
-  constructor(partial: Partial<UserResponse>) {
-    Object.assign(this, partial);
-  }
 }
 
 @Controller('user')
@@ -33,8 +19,8 @@ export class UserController {
       const userId = req.user.id;
       const user = await this.usersService.findOneById(userId);
       if (user) {
-        const userResponse = instanceToPlain(new UserResponse(user));
-        return res.json(userResponse);
+        const { password, ...userWithoutPassword } = user;
+        return res.json(userWithoutPassword);
       } else {
         return res.status(404).json({ message: 'User not found' });
       }
