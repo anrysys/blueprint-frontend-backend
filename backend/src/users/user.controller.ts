42f77a9687be1c56c54c2a2c5ecfe86e61715189
anrysys <anrysys@gmail.com>
2024-11-01 15:28:31 +0200

    Использовать instanceToPlain для преобразования ответа пользователя в контроллере

--- backend/src/users/user.controller.ts
+++ backend/src/users/user.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
-import { Exclude } from 'class-transformer'; // Импортируем class-transformer
+import { Exclude, instanceToPlain } from 'class-transformer'; // Импортируем class-transformer
 import { Request, Response } from 'express';
 import { AuthGuard } from '../auth/auth.guard';
 import { User } from './user.entity'; // Импортируем сущность User
@@ -33,7 +33,7 @@ export class UserController {
       const userId = req.user.id;
       const user = await this.usersService.findOneById(userId);
       if (user) {
-        const userResponse = new UserResponse(user);
+        const userResponse = instanceToPlain(new UserResponse(user));
         return res.json(userResponse);
       } else {
         return res.status(404).json({ message: 'User not found' });
